8f15fdea2f9717176ab2baf3b05c00cb17ce596b
Razi-abedi <razieh.a@userdata.tech>
2025-01-30 14:49:55 +0800

    edit event functionality

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -57,7 +57,7 @@ export default function EditEvent() {
           title="An error occurred"
           message={
             error.info?.message ||
-            "Failed to show event. Please try again later."
+            "Failed to show event. Please try again in a few minutes."
           }
         />
         <div className="form-actions">
