f57e3a26b5f642c74a8e2d41b3de27f04a339fdc
Razi-abedi <razieh.a@userdata.tech>
2025-01-30 14:36:10 +0800

    omtimistic updating to edit events

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,20 +1,77 @@
-import { Link, useNavigate } from 'react-router-dom';
-
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { fetchEvent, updateEvent, queryClient } from "../../util/http.js";
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const prams = useParams();
 
-  function handleSubmit(formData) {}
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", prams.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: prams.id }),
+  });
 
-  function handleClose() {
-    navigate('../');
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+      await queryClient.cancelQueries({ queryKey: ["events", prams.id] });
+      const previousEvent = queryClient.getQueryData(["events", prams.id]);
+      queryClient.setQueryData(["events", prams.id], newEvent);
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", prams.id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["events", prams.id]);
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: prams.id, event: formData });
+    navigate("../");
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  function handleClose() {
+    navigate("../");
+  }
+
+  let content;
+  if (isPending) {
+    content = (
+      <div className="center">
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock
+          title="An error occurred"
+          message={
+            error.info?.message ||
+            "Failed to load event. Please try again later."
+          }
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +79,8 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
-    </Modal>
-  );
+    );
+  }
+
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
--- src/util/http.js
+++ src/util/http.js
@@ -79,3 +79,20 @@ export async function deleteEvent({ id}) {
  return message;
  
 }
+
+export async function updateEvent({id, event}){
+const response = await fetch(`http://localhost:3000/events/${id}`, {
+  method: "PUT",
+  body: JSON.stringify({event}),
+  headers: {
+    "content-type": "application/json",
+  }
+})
+if (!response.ok) {
+  const error = new Error('An error occurred while updating the event');
+  error.code = response.status;
+  error.info = await response.json();
+  throw error;
+}
+return response.json();
+}
